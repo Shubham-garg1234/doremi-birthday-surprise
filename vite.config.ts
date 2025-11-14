3b67adc72952e468c35d04949b8233b807d49075
myusername <127469454+Shubham-garg1234@users.noreply.github.com>
2025-11-15 02:20:09 +0530

    some deploy change3

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   base: '/doremi-birthday-surprise/',
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
